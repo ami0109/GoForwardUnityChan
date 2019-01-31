c505edcee766d36d93bdc81d59c09984b2f977d5
suzukiami0109 <rfuk27@gmail.com>
2019-01-31 23:26:22 +0900

    GoForwardUnityChan Lesson7課題
    キューブが地面に衝突した時とキューブ同士が積み重なった時に効果音を鳴らすようにしました。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour
+{
+
+    // キューブの移動速度
+    private float speed = -0.2f;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        {
+            //他のキューブまたは地面と衝突した場合に音を鳴らす
+            if (collision.gameObject.tag == "Cube" || collision.gameObject.tag == "Ground")
+            {
+                GetComponent<AudioSource>().Play();
+            }
+        }
+    }
+}
+    
